fc5fa013cba6386f0e9089fd34d8966fbe9f209f
SufyanSajid <themssk@gmail.com>
2021-11-24 09:29:29 +0500

    m

--- lib/Screens/chat_screen.dart
+++ lib/Screens/chat_screen.dart
@@ -1,17 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  // void initState() {
+  //   super.initState();
+  //   Firebase.initializeApp().whenComplete(() {
+  //     print("completed hu gai");
+  //     setState(() {});
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This Works'),
-        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/CPB9ZjLHheNlGUScfqYO/messages')
+            .snapshots(),
+        builder: (ctx, AsyncSnapshot<QuerySnapshot> streamSnapShot) {
+          if (streamSnapShot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: streamSnapShot.data!.docs.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(streamSnapShot.data!.docs[index]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //  Firebase.initializeApp();
+          FirebaseFirestore.instance
+              .collection('chats/CPB9ZjLHheNlGUScfqYO/messages')
+              .add({
+            'text': 'sufyan Here',
+          });
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
